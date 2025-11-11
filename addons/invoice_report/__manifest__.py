34416dd2d17da1b73bf15a2db7934c65e31b8636
erikngigi <ericmosesngigi@gmail.com>
2025-11-11 12:07:25 +0300

    feat: add custom addons directory with bind-mounted Odoo modules
    Added a new `addons/` directory containing custom Odoo modules as bind
    mounts:
    - advance_cash_flow_statements
    - base_account_budget
    - base_accounting_kit
    - invoice_multi_approval
    - invoice_report
    - login_user_detail
    - sale_report_advanced
    - sales_order_double_approval
    - sales_report

---
+++ addons/invoice_report/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Custom Invoice Order Report',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+}
---
+++ addons/sales_report/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Custom Sale Order Report',
+    'version': '1.0',
+    'depends': ['sale'],
+    'data': [
+        'views/report_saleorder.xml',
+    ],
+    'installable': True,
+}
