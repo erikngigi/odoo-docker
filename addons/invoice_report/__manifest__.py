b035d7defe32a02ed4bfe502c00f0e93acb6f036
erikngigi <ericmosesngigi@gmail.com>
2025-11-11 15:46:31 +0300

    feat(odoo): add custom Dockerfile for Odoo 18
    - Added a new `Dockerfile` to extend the official Odoo 18 image with
      required Python dependencies (openpxyl, ofxparse, qifparse) for the
      Open Source Full Accounting Kit module by Cybrosys Techno Solutions.
    - Ensumes seamless module compatability in Docker environments without
      manual package installation

--- addons/invoice_report/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Custom Invoice Order Report',
-    'version': '1.0',
-    'depends': ['account'],
-    'data': [
-        'views/report_invoice.xml',
-    ],
-    'installable': True,
-}
--- addons/sales_report/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Custom Sale Order Report',
-    'version': '1.0',
-    'depends': ['sale'],
-    'data': [
-        'views/report_saleorder.xml',
-    ],
-    'installable': True,
-}
